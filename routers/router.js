70573fdfa2e42cd49fec43a66cf88ab8fec72856
RuhieJabbal2510 <ruhie25jabbal@gmail.com>
2023-08-21 23:46:37 +1000

    First Commit

---
+++ routers/router.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let router = express.Router();
+let controller = require('../controllers/controller');
+
+router.post('/', function(req,res){
+    // let cat = req.body;
+    // console.log(cat);
+    // insertCat(cat, (err,result) => {
+    //     if(!err){
+    //         res.json({statusCode:201,data:result,message:'success'});
+    //     }
+    // });
+
+    controller.postCat(req,res);
+});
+
+router.get('/', (req,res)=>{
+    // getAllCats((err,result)=>{
+    //     console.log(result);
+    //     if(!err){
+    //         res.json({statusCode:200,data:result,message:'success'});
+    //     }
+    // });
+
+    controller.getAllCats(req,res);
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,66 @@
+let express = require('express');
+let app = express();
+require('./dBConnection'); 
+let router = require('./routers/router');
+let port = process.env.port || 3000;
+//const { MongoClient, ServerApiVersion } = require('mongodb');
+//const uri = "mongodb+srv://ruhie2510:ruhie2510@cluster0.b5gegtd.mongodb.net/?retryWrites=true&w=majority";
+//let collection;
+
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/api/cat',router);
+
+/*async function run() {
+    try {
+      await client.connect();
+      collection = client.db().collection('Cats');
+      console.log(collection);
+    } catch(ex) {
+      console.error(ex);
+    }
+}
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+
+app.get('/', function (req,res) {
+    res.render('index.html');
+});
+
+app.post('/api/cat', function(req,res){
+    // ask DB to post this CAT
+    let cat = req.body;
+    insertCat(cat, (err,result) => {
+        if (!err) {
+            res.json({statusCode:201,data:result,message:'success'});
+        }
+    });
+});
+
+function insertCat(cat, callback) {
+    collection.insertOne(cat,callback);
+}
+
+app.get('/api/cats', (req,res)=>{
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+});
+
+function getAllCats(callback) {
+    collection.find({}).toArray(callback);
+}*/
+
+app.listen(port, ()=>{
+    console.log('express server started');
+    
+});
